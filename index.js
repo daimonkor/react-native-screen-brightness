efacb3d5c7408ce7262b2460d14e87fa96df183f
dimakorniets <dimakorniets@gmail.com>
2020-05-30 11:39:14 +0300

    Refactor getBrightness to 0-1 (Android)

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import {NativeModules, Platform} from 'react-native';
 export default NativeModules.ScreenBrightness;
 
 const oldHasPermission = NativeModules.ScreenBrightness.hasPermission
-
 NativeModules.ScreenBrightness.hasPermission = async () => {
-    console.log("modified check permission")
+    console.log("call modified check permission")
     if (Platform.OS === "android") {
         return oldHasPermission();
     }
@@ -15,9 +14,8 @@ NativeModules.ScreenBrightness.hasPermission = async () => {
 }
 
 const oldGetScreenBrightnessMode = NativeModules.ScreenBrightness.getScreenBrightnessMode
-
 NativeModules.ScreenBrightness.getScreenBrightnessMode = async () => {
-    console.log("modified getScreenBrightnessMode")
+    console.log("call modified getScreenBrightnessMode")
     if (Platform.OS === "android") {
         return oldGetScreenBrightnessMode();
     }
@@ -27,13 +25,27 @@ NativeModules.ScreenBrightness.getScreenBrightnessMode = async () => {
 }
 
 const oldSetScreenBrightnessMode = NativeModules.ScreenBrightness.setScreenBrightnessMode
-
 NativeModules.ScreenBrightness.setScreenBrightnessMode = (value) => {
-    console.log("modified setScreenBrightnessMode")
+    console.log("call modified setScreenBrightnessMode")
     if (Platform.OS === "android") {
         return oldSetScreenBrightnessMode(value);
     }
     else{
         return Promise.resolve(1)
+    }
+}
+
+const oldGetBrightness = NativeModules.ScreenBrightness.getBrightness
+NativeModules.ScreenBrightness.getBrightness = () => {
+    console.log("call modified getBrightness")
+    if (Platform.OS === "android") {
+        return new Promise(((resolve, reject) => {
+            oldGetBrightness().then((value) => {
+                return resolve(value / 255)
+            }).catch(reject)
+        }));
+    }
+    else{
+        return oldGetBrightness()
     }
 }
